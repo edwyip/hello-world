d974f5e401b38fef9f40bcb463d7124a2f78d47d
EdwardYip <clyip93@gmail.com>
2017-03-12 07:16:40 +0000

    added swap.c

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 8;
+    printf("a = %i, b = %i\n", a, b);
+    
+    printf("swapping...\n");
+    
+    printf("swapped(not really)\n");
+    
+    printf("a = %i, b = %i\n", a, b);    
+}
